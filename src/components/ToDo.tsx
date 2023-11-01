85e029bfb120d047833c05ca0dbc19b844720453
hyejikng <hyeji8842@gmail.com>
2023-11-02 02:29:49 +0900

    set cagegories

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,14 +1,34 @@
-import { IToDo } from './atoms';
+import { useSetRecoilState } from 'recoil';
+import { IToDo, toDoState } from './atoms';
 
-function ToDo({ text, category }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    // console.log(event.currentTarget.name);
+    const {
+      currentTarget: { name },
+    } = event;
+  };
   return (
     <ul>
       <li>
         <span>{text}</span>
-        {category !== 'DOING' && <button>Doing</button>}
+        {category !== 'DOING' && (
+          <button name="DOING" onClick={onClick}>
+            Doing
+          </button>
+        )}
         {/* 카테고리가 doing이 아닐때에만 Doing 버튼을 보여줄 것이다.  */}
-        {category !== 'TO_DO' && <button>To Do</button>}
-        {category !== 'DONE' && <button>Done</button>}
+        {category !== 'TO_DO' && (
+          <button name="TO_DO" onClick={onClick}>
+            To Do
+          </button>
+        )}
+        {category !== 'DONE' && (
+          <button name="DONE" onClick={onClick}>
+            Done
+          </button>
+        )}
       </li>
     </ul>
   );

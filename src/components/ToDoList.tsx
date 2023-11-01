81267a87667b8333a8bdbc935d4e73a6f905468c
hyejikng <hyeji8842@gmail.com>
2023-11-02 02:48:40 +0900

    immutablility part one

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -5,11 +5,13 @@ import ToDo from './ToDo';
 
 function Todolist() {
   const toDos = useRecoilValue(toDoState);
+  // console.log(toDos);
   return (
     <div>
       <h1>TO DO LIST</h1>
       <hr />
       <CreateToDo />
+
       <ul>
         {toDos.map((item) => (
           <ToDo key={item.id} {...item} /> // {...item} => interface IToDo
